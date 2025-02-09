ce41af88b9c8301630a78bdff88c60922d57642c
Vishal Rao <raovishal487@gmail.com>
2025-02-09 13:14:51 +0530

    dockerize: frontend working fine

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api/v1', // Fallback to localhost if env var is not set
 });
 
 export const fetchAllFeatures = async () => {
