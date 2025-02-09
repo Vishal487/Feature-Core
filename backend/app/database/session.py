5be468741472bfd7e2089fc3387838a85b780586
Vishal Rao <raovishal487@gmail.com>
2025-02-09 10:00:31 +0530

    Dockerfiles and docker-compose: working fine for backend and database

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -1,12 +1,13 @@
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from app.database.models import Base  # Import Base from models
 
-load_dotenv()
+# load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DEFAULT_DB_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/feature_core"
+DATABASE_URL = os.getenv("DATABASE_URL", default=DEFAULT_DB_URL)
 
 engine = create_async_engine(DATABASE_URL)
 AsyncSessionLocal = sessionmaker(
